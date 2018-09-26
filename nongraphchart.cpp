f35ddeab83d9f4581803c395ff85e454e3249492
Witless <witlesshydra@outlook.com>
2018-09-26 22:09:47 +0200

    Marco de trabajo

---
+++ nongraphchart.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main ()
+
+{
+	unsigned i=0, j, k, l, m, n, o, p, q;
+	
+	
+	
+	
+	// Esto sería sólo para una vez obtenidos los datos de la primera fila, generar una especie de tabla 
+
+   printf("%i | %i | %i | %i | %i | %i | %i | %i | %i\n", i, j, k, l, m, n, o, p, q);
+   
+   //A partir de aquí se volverían a manipular las variables para imprimirlas en la siguiente fila y sucesivamente
+}    
+
+
+
+
+
+
